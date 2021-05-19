53901da11055be7b340e5654dce27d3e9038ef0e
guillermomolina <guillermoadrianmolina@hotmail.com>
2021-05-19 13:21:32 +0200

    dev

--- imports/components/SubForm.svelte
+++ imports/components/SubForm.svelte
@@ -1,25 +1,25 @@
 <script>
-  import {
-    Card,
-    CardHeader,
-    CardBody
-  } from "sveltestrap";
-  import FormGroupText from "./FormGroupText.svelte";
+  import { Card, CardHeader, CardBody } from 'sveltestrap';
+  import FormInput from './FormInput.svelte';
 
   export let schema;
+  export let tittle;
+  export let key;
   export let object;
 
   object = object || {};
-  $: value = key in object? object[key]: ''; 
+  $: value = key in object? object[key]: {}; 
 
   const setValue = () => {
     object[key] = value;
   }
-
 </script>
 
-<Card class="border-0">
-  <CardHeader class="bg-transparent">Operating system</CardHeader>
+<Card class='border-0'>
+  <CardHeader class='bg-transparent'>{tittle}</CardHeader>
   <CardBody>
+    {#each schema._schemaKeys as subkey}
+      <FormInput {schema} key={subkey} bind:object={object} />
+    {/each}
   </CardBody>
 </Card>
